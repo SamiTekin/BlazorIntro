5833fd4a67bd9d898583f7da49a2895c80323b2f
Sami Tekin <samii.tekiin@gmail.com>
2024-09-04 18:26:22 +0300

    Add project files.

---
+++ BlazorIntro/Components/Pager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BlazorIntro.Components;
+
+public partial class Pager:ComponentBase
+{
+    [Parameter]
+    public int TotalItems { get; set; }
+
+    [Parameter]
+    public int ItemsPerPage { get; set; }
+
+    [Parameter]
+    public int CurrentPage { get; set; }
+
+    [Parameter]
+    public EventCallback<int> OnPageChange { get; set; }
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    public int TotalPages => (int)Math.Ceiling((double)TotalItems / ItemsPerPage);
+
+    private async Task HandlePageChange(int page)
+    {
+        await OnPageChange.InvokeAsync(page);
+    }
+}
